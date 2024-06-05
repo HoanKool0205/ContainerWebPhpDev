ee1f50d37caae22a10bc5f20b9a1e58468864fed
HoanKool0205 <hoanlombard0205@gmail.com>
2024-06-05 23:51:25 +0700

    Sale_Price - phiếu giảm giá thay đổi cả giá trong product

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -18,6 +18,7 @@ class HomeComponent extends Component
         $cats = explode(',',$categogy->sel_categories);
         $categories = Category::whereIn('id',$cats)->get();
         $no_of_products = $categogy->no_of_products;
-        return view('livewire.home-component',['sliders'=>$slider,'lproducts'=>$lproducts,'categories'=>$categories,'no_of_products'=>$no_of_products])->layout('layouts.base');
+        $sproducts = Product::where('sale_price','>',0)->inRandomOrder()->get()->take(8);
+        return view('livewire.home-component',['sliders'=>$slider,'lproducts'=>$lproducts,'categories'=>$categories,'no_of_products'=>$no_of_products,'sproducts'=>$sproducts])->layout('layouts.base');
     }
 }
