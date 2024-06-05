19d8881314effb367f0ea1a545206f58fd1b8652
HoanKool0205 <hoanlombard0205@gmail.com>
2024-06-05 21:10:29 +0700

    tạo bộ lọc product cho Product Categries bộ lọc sản phẩm phí trên sản phẩm

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
+use App\Models\HomeCategory;
 use App\Models\HomeSlider;
 use App\Models\Product;
 use Livewire\Component;
@@ -12,6 +14,10 @@ class HomeComponent extends Component
     {
         $slider = HomeSlider::where('status',1)->get();
         $lproducts = Product::orderBy('created_at','DESC')->get()->take(8);
-        return view('livewire.home-component',['sliders'=>$slider,'lproducts'=>$lproducts])->layout('layouts.base');
+        $categogy = HomeCategory::find(1);
+        $cats = explode(',',$categogy->sel_categories);
+        $categories = Category::whereIn('id',$cats)->get();
+        $no_of_products = $categogy->no_of_products;
+        return view('livewire.home-component',['sliders'=>$slider,'lproducts'=>$lproducts,'categories'=>$categories,'no_of_products'=>$no_of_products])->layout('layouts.base');
     }
 }
