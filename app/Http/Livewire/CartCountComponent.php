13009659787ad07430fdd8186048dbd7f84702e2
HoanKool0205 <hoanlombard0205@gmail.com>
2024-06-10 22:04:29 +0700

    lấy số lượng sản phẩm từ trang thông tin để add vào giỏ hàng

--- app/Http/Livewire/CartCountComponent.php
+++ app/Http/Livewire/CartCountComponent.php
@@ -8,6 +8,11 @@ class CartCountComponent extends Component
 {
     protected $listeners = ['refreshComponent' =>'refresh'];
 
+    public function refresh()
+    {
+        // This method allows the component to be refreshed when called.
+    }
+
     public function render()
     {
         return view('livewire.cart-count-component');
