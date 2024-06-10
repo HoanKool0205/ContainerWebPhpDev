7696ec0798beb0c8c31b201f328be96d99087abb
HoanKool0205 <hoanlombard0205@gmail.com>
2024-06-10 23:00:55 +0700

    save product to cart if you don't buy product

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -50,6 +50,7 @@
 										<a class="btn btn-increase" href="#" wire:click.prevent="increaseQuantity('{{ $item->rowId }}')"></a>
 										<a class="btn btn-reduce" href="#" wire:click.prevent="decreaseQuantity('{{ $item->rowId }}')"></a>
 									</div>
+									<p class="text-center"><a href="#" wire:click.prevent="switchToSaveForLater('{{$item->rowId}}')">Save For Later</a></p>
 								</div>
 								<div class="price-field sub-total">
 									<p class="price">${{ $item->subtotal }}</p>
@@ -87,6 +88,58 @@
 						<a class="btn btn-clear" href="#" wire:click.prevent="destroyAll()" >Clear Shopping Cart</a>
 						<a class="btn btn-update" href="#">Update Shopping Cart</a>
 					</div>
+				</div>
+
+				<div class="wrap-iten-in-cart">
+					<h3 class = "title-box" style="border-bottom: 1px solid; padding-bottom:15px">{{Cart::instance('saveForLater')->count()}} item(s) Saved For Later</h3>
+					@if(Session::has('s_success_message'))
+						<div class="alert alert-success">
+							<strong>Success</strong> {{Session::get('s_success_message')}}
+						</div>
+					@endif
+					@if(Cart::instance('saveForLater')->count() > 0)
+					<h3 class="box-title">Products Name</h3>
+					<ul class="products-cart">
+						@foreach (Cart::instance('saveForLater')->content() as $item)
+							<li class="pr-cart-item">
+								<div class="product-image">
+									<figure>
+										@if(isset($item->model->image))
+											<img src="{{ asset('assets/images/products') }}/{{ $item->model->image }}" alt="{{ $item->model->name }}">
+										@else
+											<img src="{{ asset('assets/images/default.png') }}" alt="Default Image">
+										@endif
+									</figure>
+								</div>
+								<div class="product-name">
+									@if(isset($item->model->name) && isset($item->model->slug))
+										<a class="link-to-product" href="{{ route('product.details', ['slug' => $item->model->slug]) }}">{{ $item->model->name }}</a>
+									@else
+										<span>No product name available</span>
+									@endif
+								</div>
+								<div class="price-field produtc-price">
+									@if(isset($item->model->regular_price))
+										<p class="price">${{ $item->model->regular_price }}</p>
+									@else
+										<p class="price">N/A</p>
+									@endif
+								</div>
+								<div class="quantity">
+									<p class="text-center"><a href="#" wire:click.prevent="moveToCart('{{$item->rowId}}')">Move To Cart</a></p>
+								</div>
+								<div class="delete">
+									<a href="#" wire:click.prevent="deleteFromSaveForLater('{{ $item->rowId }}')" class="btn btn-delete" title="">
+										<span>Delete from save for later</span>
+										<i class="fa fa-times-circle" aria-hidden="true"></i>
+									</a>
+								</div>
+							</li>
+						@endforeach													
+					</ul>
+					@else
+						<p>No item save for later</p>
+					@endif
 				</div>
 
 				<div class="wrap-show-advance-info-box style-1 box-in-site">
