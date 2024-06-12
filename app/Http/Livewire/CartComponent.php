23cf88283e3ecd495c1b6d4a331637228bfc94c6
HoanKool0205 <hoanlombard0205@gmail.com>
2024-06-12 18:51:32 +0700

    checkout buy product

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -5,7 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\Coupon;
 use Livewire\Component;
 use Cart;
-
+use Illuminate\Support\Facades\Auth;
 
 class CartComponent extends Component
 {
@@ -96,8 +96,42 @@ class CartComponent extends Component
         }
     }
 
-    public function removeCoupon(){
+    public function removeCoupon()
+    {
         session()->forget('coupon');
+    }
+
+    public function checkout()
+    {
+        if(Auth::check())
+        {
+            return redirect()->route('checkout');
+        }
+        else{
+            return redirect()->route('login');
+        }
+    }
+
+    public function setAmountForCheckout()
+    {
+        if(session()->has('coupon'))
+        {
+            session()->put('checkout',[
+                'discount' => $this->discount,
+                'subtotal' => $this->subtotalAfterDiscount,
+                'tax' => $this->taxAfterDiscount,
+                'total' => $this->totalAfterDiscount
+            ]);
+        }
+        else
+        {
+            session()->put('checkout',[
+                'discount' => 0,
+                'subtotal' => Cart::instance('cart')->subtotal(),
+                'tax' => Cart::instance('cart')->tax(),
+                'total' => Cart::instance('cart')->total()
+            ]);
+        }
     }
 
     public function render()
@@ -110,6 +144,7 @@ class CartComponent extends Component
                 $this->calculateDiscounts();
             }
         }
+        $this->setAmountForCheckout();
         return view('livewire.cart-component')->layout("layouts.base");
     }
 }
