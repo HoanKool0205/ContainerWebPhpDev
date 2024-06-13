84d124cc88fc3c3bff9adc8852b6e1ac7fe97795
HoanKool0205 <hoanlombard0205@gmail.com>
2024-06-13 11:02:22 +0700

    Admin hiển thị chi tiết đơn đặt hàng

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -18,7 +18,7 @@ class Order extends Model
 
     public function orderItems()
     {
-        return $this->hamany(OrderItem::class);
+        return $this->hasMany(OrderItem::class);
     }
 
     public function shipping()
